4316e33e77f4251b1cb6f55fd99e6677a6895949
秃头艾 <2240280094@qq.com>
2023-08-16 19:46:03 +0800

    练习

---
+++ 2023.8.16/2023.8.16/源.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//²éÕÒ×óĞı×Ö·û´®
+//int is_left_move(char arr1[], char arr2[])
+//{
+//	int len1 = strlen(arr1);
+//	int len2 = strlen(arr2);
+//	if (len1 != len2)
+//		return 0;
+//	strncat(arr1, arr1, len1);
+//	char* ret = strstr(arr1, arr2);
+//	if (ret == NULL)
+//		return 0;
+//	else
+//		return 1;
+//}
+
+int main()
+{
+	int n = 0;
+	int m = 0;
+	scanf("%d%d", &n, &m);
+	int arr[10][10] = { 0 };
+	int i = 0;
+	int j = 0;
+	for(i)
+
+	return 0;
+}

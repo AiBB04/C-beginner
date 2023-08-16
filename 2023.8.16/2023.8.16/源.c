b8118082ac4c5c531447971ea4e94f0917d5e71c
秃头艾 <2240280094@qq.com>
2023-08-16 22:22:25 +0800

    练习

--- 2023.8.16/2023.8.16/源.c
+++ 2023.8.16/2023.8.16/源.c
@@ -18,15 +18,38 @@
 //		return 1;
 //}
 
-int main()
-{
-	int n = 0;
-	int m = 0;
-	scanf("%d%d", &n, &m);
-	int arr[10][10] = { 0 };
-	int i = 0;
-	int j = 0;
-	for(i)
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[10][10];
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < n;i++)
+//	{
+//		for (j = 0;j < n;j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	//ÅĞ¶Ï
+//	int flag = 1;
+//	for (i = 0;i < n;i++)
+//	{
+//		for (j = 0;j < i;j++)
+//		{
+//			if (arr[i][j] != 0)
+//			{
+//				flag = 0;
+//				goto end;
+//			}
+//		}
+//	}
+//end:
+//	if (flag == 0)
+//		printf("N0\n");
+//	else
+//		printf("Yes\n");
+//	return 0;
+//}
 
-	return 0;
-}

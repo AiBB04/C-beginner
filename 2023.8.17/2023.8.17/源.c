d542474882af732aff1171158712b054633fa2ef
秃头艾 <2240280094@qq.com>
2023-08-18 04:36:10 +0800

    练习

--- 2023.8.17/2023.8.17/源.c
+++ 2023.8.17/2023.8.17/源.c
@@ -159,3 +159,26 @@
 //
 //	return 0;
 //}
+
+//结构体内存对齐
+//struct S1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//struct S2
+//{
+//	char c1;
+//	char c2;
+//	int i;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct S1));
+//	printf("%d\n", sizeof(struct S2));
+//
+//	return 0;
+//}
+
